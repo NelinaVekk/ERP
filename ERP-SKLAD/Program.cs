5ec185b93315c20e55ecd72e9484700feb735854
kr1skyyy <kr1skyyy@abv.bg>
2021-05-23 15:31:53 +0300

    Hello world

--- ERP-SKLAD/Program.cs
+++ ERP-SKLAD/Program.cs
@@ -17,6 +17,7 @@ namespace ERP_SKLAD
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Hello world!");
         }
     }
 }
